2a886a4706c0c421161a0535b1249e069e8364cd
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2025-10-13 21:52:52 +0800

    commit 3

--- connections.php
+++ connections.php
@@ -1,6 +1,6 @@
 <?php
 
-$connections = mysqli_connect("localhost","root","","ereklamo_dbv1");
+$connections = mysqli_connect("mysql.hostinger.com","u815942348_eReklamodbv1","@eReklamo12345","u815942348_ereklamodbv1");
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
