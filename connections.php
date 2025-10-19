6b7661222148a90d4514de5bfea3f2bd218b4d34
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2025-10-20 01:28:40 +0800

    Updates 2 1AM

--- connections.php
+++ connections.php
@@ -1,6 +1,6 @@
 <?php
 
-$$connections = mysqli_connect("mysql.hostinger.com","u815942348_eReklamodbv1","@eReklamo12345","u815942348_ereklamodbv1");
+$connections = mysqli_connect("mysql.hostinger.com","u815942348_eReklamodbv1","@eReklamo12345","u815942348_ereklamodbv1");
 #connections = mysqli_connect("localhost","root","","ereklamo_dbv2");
 
 if(mysqli_connect_errno()) {
