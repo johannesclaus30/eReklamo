caee1324bfabdf50dffd35284bf86e98a2ce989c
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2025-10-20 08:29:22 +0800

    8AM Update

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -51,13 +51,13 @@ if (!isset($_SESSION['User_ID'])) {
                             </div>
                         </div>
                     </div>
-                    <button class="btn btn-outline-white" onclick="window.location.href='../sign_in'">
+                    <button class="btn btn-outline-white" onclick="window.location.href='../signout'">
                         <svg class="btn-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                             <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                             <polyline points="16 17 21 12 16 7"></polyline>
                             <line x1="21" y1="12" x2="9" y2="12"></line>
                         </svg>
-                        Logout
+                        Sign Out
                     </button>
                 </div>
             </div>
